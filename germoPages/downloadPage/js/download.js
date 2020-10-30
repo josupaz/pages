ccaaebc732c586958ae49a7ef24a7d993c7c3930
josupaz <54748658+josupaz@users.noreply.github.com>
2020-10-30 16:25:16 -0300

    Version 1.0

--- germoPages/downloadPage/js/download.js
+++ germoPages/downloadPage/js/download.js
@@ -1,63 +1,145 @@
+//--------------------------------------------------------------------Downloads Page--------------------------------------------------------------------
+//
+// Para crear una nueva tarjeta agregar un nuevo elemento a los arrays ubicado en VARIABLES GLOBALES y RESPETAR LO SIGUIENTE:
+//
+// Title        | Size: min: 1 max: 23 caracteres. [a partir de 12 caracteres, debe ser >= a 2 palabras].
+// Descripcion  | Size: min: 1 max: 100 caracteres.
+// Img          | Size: 100 x 100 px.
+//
+//---------------------------------------------------------------------Version 1.0---------------------------------------------------------------------
+
+//---------------------(--VAR--GLOBALES--)----------------------
+var titles = ["Office 2016",
+  "KMSAuto 1.5.3",
+  "Anydesk Particulares",
+  "Anydesk didot",
+  "Anydesk Mega"];
+
+var imgs = [
+  "https://img.icons8.com/bubbles/100/000000/office-365.png",
+  "https://img.icons8.com/color/100/000000/windows8.png",
+  "https://img.icons8.com/color/100/000000/anydesk.png",
+  "https://img.icons8.com/color/100/000000/anydesk.png",
+  "https://img.icons8.com/color/100/000000/anydesk.png"];
+
+var descriptions = [
+  "Sin descripcion",
+  "Sin descripcion",
+  "Sin descripcion",
+  "Sin descripcion",
+  "Sin descripcion"];
+
+var urls = [
+  "https://drive.google.com/file/d/1pQu1umdLCKVRnbtdO78_nP3A7WrtWCqF/view?usp=sharin",
+  "http://dl.xytech.com.ar/KMSAUTO.rar",
+  "https://get.anydesk.com/j7fO0983/AnyDesk_XYTech_Particulares.exe",
+  "http://dl.xytech.com.ar/anydesk-clients/AnyDesk_XYTech_Client_didot.exe",
+  "http://dl.xytech.com.ar/anydesk-clients/AnyDesk_XYTech_Client_mega.exe"];
+
+ 
+//--------------(--FIN--VAR--GLOBALES--)----------------------
+var cantDeApps = titles.length;
+var i;
+
+createCard();
+createSwitch();
+
 
 // /* Interruptor ON/OFF */ 
-document.getElementsByClassName("slider round").onclick = function() {myFunction()};
-
-function myFunction() {
-
-  // /* CANTIDAD DE APP - SI SE AGREGA UNA APP SE DEBE SUMAR AQUI */
-  var cantidadDeApps = 5;
-  var i;
-
-  if(document.getElementById("clase-con-id0").classList.contains("col-lg-4")){
-    //itero hasta la cantidad de apps y cambio los estilos vista-detalle
-    for( i = 0 ; i < cantidadDeApps; i++ ){
-
-      document.getElementById("clase-con-id"+i).classList.add("col-lg-12");
-      document.getElementById("clase-con-id"+i).classList.remove("col-lg-4");
-    
-      document.getElementById("card-detail"+i).classList.remove("card");
-      document.getElementById("card-detail"+i).classList.add("detail");
-      
-      document.getElementById("title-card-detail"+i).classList.remove("title-card");
-      document.getElementById("title-card-detail"+i).classList.add("title-card-detail");
-    
-      document.getElementById("img-card-detail"+i).classList.remove("img-card");
-      document.getElementById("img-card-detail"+i).classList.add("img-card-detail");
-    
-      document.getElementById("description-card-detail"+i).classList.remove("description-card");
-      document.getElementById("description-card-detail"+i).classList.add("description-card-detail");
-    
-      document.getElementById("box-btn-detail"+i).classList.remove("box-btn");
-      document.getElementById("box-btn-detail"+i).classList.add("box-btn-detail");
-    }
-
-  }else{
+document.getElementsByClassName("slider round").onclick = function () { myFunction() };
 
 
-        for( i = 0 ; i < cantidadDeApps; i++ ){
 
-      document.getElementById("clase-con-id"+i).classList.remove("col-lg-12");
-      document.getElementById("clase-con-id"+i).classList.add("col-lg-4");
-    
-      document.getElementById("card-detail"+i).classList.add("card");
-      document.getElementById("card-detail"+i).classList.remove("detail");
-      
-      document.getElementById("title-card-detail"+i).classList.add("title-card");
-      document.getElementById("title-card-detail"+i).classList.remove("title-card-detail");
-    
-      document.getElementById("img-card-detail"+i).classList.add("img-card");
-      document.getElementById("img-card-detail"+i).classList.remove("img-card-detail");
-    
-      document.getElementById("description-card-detail"+i).classList.add("description-card");
-      document.getElementById("description-card-detail"+i).classList.remove("description-card-detail");
-    
-      document.getElementById("box-btn-detail"+i).classList.add("box-btn");
-      document.getElementById("box-btn-detail"+i).classList.remove("box-btn-detail");
-    }
-    
+
+
+
+
+// CREACION DE TARJETA
+function createCard() {
+  var row = document.getElementById("row");
+
+  for (i = 0; i < cantDeApps; i++) {
+    row.innerHTML += `
+          
+      <div id="bootsId`+ i + `" class="col-sm-12 col-md-6 col-lg-4">
+      <div id="card`+ i + `"class="card">
+        <span id="title-card`+ i + `" class="title-card">${titles[i]}</span>
+         <img id="img-card`+ i + `"class="img-card" src="${imgs[i]}"/>
+         <span id="description-card`+ i + `"class="desctription-card">${descriptions[i]}</span>
+         <a id="box-btn`+ i + `" href="${urls[i]}"  target="_blank" class="box-btn">Download</a>
+      </div>
+      </div>`;
   }
+
+}
+
+// CREACION DE SWITCH
+function createSwitch() {
+  var switchV = document.getElementById("switch-general");
+  switchV.innerHTML += `
+          
+  <img class="icons-general" src="https://img.icons8.com/ios-glyphs/30/000000/bank-cards.png"/>
+  <label class="switch" >
+  <input type="checkbox">
+  <div class="slider round"  onclick="myFunction()"></div>
+    </label>
+    <img class="icons-general" src="https://img.icons8.com/metro/26/000000/list.png"/> 
+    `;
 }
 
 
+// /* Interruptor ON/OFF */ 
+function myFunction() {
 
+  if (document.getElementById("bootsId0").classList.contains("col-lg-4")) {
+
+    //itero hasta la cantidad de apps y cambio los estilos vista-detalle
+    for (i = 0; i < cantDeApps; i++) {
+
+      document.getElementById("bootsId" + i).classList.remove("col-lg-4");
+      document.getElementById("bootsId" + i).classList.add("col-lg-12");
+
+      document.getElementById("card" + i).classList.remove("card");
+      document.getElementById("card" + i).classList.add("card-detail");
+
+      document.getElementById("title-card" + i).classList.remove("title-card");
+      document.getElementById("title-card" + i).classList.add("title-card-detail");
+
+      document.getElementById("img-card" + i).classList.remove("img-card");
+      document.getElementById("img-card" + i).classList.add("img-card-detail");
+
+      document.getElementById("description-card" + i).classList.remove("description-card");
+      document.getElementById("description-card" + i).classList.add("description-card-detail");
+
+      document.getElementById("box-btn" + i).classList.remove("box-btn");
+      document.getElementById("box-btn" + i).classList.add("box-btn-detail");
+    }
+
+  } else {
+
+
+    for (i = 0; i < cantDeApps; i++) {
+
+      document.getElementById("bootsId" + i).classList.remove("col-lg-12");
+      document.getElementById("bootsId" + i).classList.add("col-lg-4");
+
+      document.getElementById("card" + i).classList.add("card");
+      document.getElementById("card" + i).classList.remove("card-detail");
+
+      document.getElementById("title-card" + i).classList.add("title-card");
+      document.getElementById("title-card" + i).classList.remove("title-card-detail");
+
+      document.getElementById("img-card" + i).classList.add("img-card");
+      document.getElementById("img-card" + i).classList.remove("img-card-detail");
+
+      document.getElementById("description-card" + i).classList.add("description-card");
+      document.getElementById("description-card" + i).classList.remove("description-card-detail");
+
+      document.getElementById("box-btn" + i).classList.add("box-btn");
+      document.getElementById("box-btn" + i).classList.remove("box-btn-detail");
+    }
+
+  }
+
+}
 
